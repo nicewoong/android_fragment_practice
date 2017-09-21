2ad81c8dcedce416394eda5d4fbd105197a8b7f4
nicewoong <thoughtscdoe@gmail.com>
2017-09-21 10:16:07 +0900

    Completed writing WordsFragment Class methods

--- app/src/main/java/com/example/administrator/fragmenttest3/WordsFragment.java
+++ app/src/main/java/com/example/administrator/fragmenttest3/WordsFragment.java
@@ -1,7 +1,12 @@
 package com.example.administrator.fragmenttest3;
-
-import android.app.ListFragment;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ListFragment;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -16,6 +21,43 @@ public class WordsFragment extends ListFragment {
     public interface OnWordSelectedListener {
         public void onWordSelected(int position);
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int layout = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB
+                ? android.R.layout.simple_list_item_activated_1
+                : android.R.layout.simple_list_item_1;
+
+        setListAdapter(new ArrayAdapter<String>(getActivity(), layout, Data.words));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (getFragmentManager().findFragmentById(R.id.definition_fragment) != null) {
+            getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mCallback = (OnWordSelectedListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnWordSelectedListener");
+        }
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        mCallback.onWordSelected(position);
+        getListView().setItemChecked(position, true);
+    }
+
+
 
 
 }
